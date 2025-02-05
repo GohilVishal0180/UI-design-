e4bac5b9b9888d3c7e9e68b9fbd83aaac1b0ef1e
Gohil Vishalbhai <gohilvishal7600@gmail.com>
2025-02-05 09:55:18 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+/digital-market
+  /pages
+    index.js        # Your homepage
+    api/
+      products.js    # API to fetch products
+    _app.js          # Your Next.js App file
+  /components
+  /models
+  /utils
+  .env
+  package.json
+  tailwind.config.js
+  ...
